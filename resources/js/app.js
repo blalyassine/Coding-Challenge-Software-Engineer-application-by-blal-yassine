61fe44eaa70b486563a86925256e1d1bc1286c63
blal yassine <blalyassin10@gmail.com>
2021-02-28 16:08:24 +0100

    conect componet with home

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
+
 require('./bootstrap');
 import Vue from 'vue';
 window.Vue = require('vue');
 
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
 const app = new Vue({
-    el: '#app',
+    el: '#app'
 });
+
